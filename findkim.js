f1d543894301e3e2c83e48d72e1c0a1eb278c1fa
Rootjang92 <janggeunho7@gmail.com>
2018-05-19 17:02:01 +0900

    알고리즘 1번 문제

--- findkim.js
+++ findkim.js
@@ -5,7 +5,7 @@ function findkim(seoul){
       index = i;
     }
   }
-
+// for문 설정하면서 변수 i를 선언하여 seoul 배열의 길이를 받아옴.
 return "kim은 " + index + "번 째에 있다.";
 }
 console.log(findkim(["Lee", "park", "kim"]))
