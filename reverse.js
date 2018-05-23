8e44beebb771e81c3f8bc5262831187e4f1a6e93
Rootjang92 <janggeunho7@gmail.com>
2018-05-23 09:51:09 +0900

    how to act for function

--- reverse.js
+++ reverse.js
@@ -2,7 +2,7 @@ function reverse(str){
   var string = "";
   // var strReverse = string.split(',');
   for (var i in str){
-    // document.write('<p>' + strReverse[i] + '</p>')
+    // for(var i=0; i<str.length-1-i; i++)
     string += str[str.length-1-i];
   }
   return string;
