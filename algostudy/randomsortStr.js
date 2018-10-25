95e9d09e12fa53b0ec6442514b635d0abd99884d
Rootjang92 <janggeunho7@gmail.com>
2018-10-25 21:34:51 +0900

    문자열 내 마음대로 정렬하기

---
+++ algostudy/randomsortStr.js
@@ -0,0 +1,11 @@
+// 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다.
+// 예를 들어 strings가 [sun, bed, car]이고 n이 1이면 각 단어의 인덱스 1의 문자 u, e, a로 strings를 정렬합니다.
+
+// localeCompare()
+// 기준 문자열과 비교했을 때 비교 대상 문자열이 정렬상 전에 오는지, 후에 오는지 혹은 같은 순서에 배치되는지를 알려주는 숫자를 리턴합니다.
+
+function solution(strings, n) {
+  return strings.sort((a,b) => a[n] ===  b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
+};
+
+console.log(solution(['sun', 'bed', 'car'],1));
