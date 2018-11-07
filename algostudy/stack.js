1bdb09c43afa42635ad3a1f72266c2f3f9661e29
Rootjang92 <janggeunho7@gmail.com>
2018-11-07 14:58:06 +0900

    stack for es6 class

---
+++ algostudy/stack.js
@@ -0,0 +1,23 @@
+class Stack {
+  constructor() {
+    this._arr = [];
+  }
+  push(item) {
+    this._arr.push[item];
+  }
+  pop() {
+    return this._arr.pop();
+  }
+  peek() {
+    return this._arr[this._arr.length];
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+stack.pop();
+
+
