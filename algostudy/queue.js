4c509876d2da744c458586046bfa0fda8baaa303
Rootjang92 <janggeunho7@gmail.com>
2018-11-07 15:01:24 +0900

    queue for es6 class

---
+++ algostudy/queue.js
@@ -0,0 +1,18 @@
+class Queue {
+  constructor() {
+    this._arr = [];
+  }
+
+  enqueue(item) {
+    this._arr.push(item);
+  }
+
+  dequeue() {
+    return this._arr.shift();
+  }
+}
+
+const queue = new Queue;
+queue.enqueue(1);
+queue.enqueue(2);
+queue.dequeue();
